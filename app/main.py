01f1c3efeb224a5b440e2a0fb79f23a682a3cac5
ram <choco@rams-MacBook-Air-2.local>
2025-05-31 22:42:30 +0800

    Initialized project skeleton, folder hierarchy and a FastAPI app in main.py

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI # class used to create a my web application
+
+app = FastAPI(title="ML Prediction Explanation Interface") # Initializing my app as an instance of the FastAPI class
+
+@app.get("/") # route decorator - home page of API
+def read_root():
+    return {
+        "message": "API is working"
+        }
+
+# uvicorn app.main:app --reload 
+'''
+    uvicorn --> The ASGI server that runs your app
+
+
+'''

bae7dd03e922786d8f98358691cc87a9b69eb63d
harshanashwad <harshanashwad@gmail.com>
2025-06-11 21:59:13 +0800

    feat(shap): added local feature importances in json response (regression task)

--- app/ml_core/explain.py
+++ app/ml_core/explain.py
@@ -25,12 +25,14 @@ def get_feature_group_map(columns):
         mapping[base].append(col)
     return mapping
 
-def group_global_shap_values(global_vals, columns):
+def group_global_shap_values(global_vals, columns, task):
     mapping = get_feature_group_map(columns)
     grouped = []
+    print(global_vals.shape)
     for base_feature, subcols in mapping.items():
         indices = [columns.get_loc(c) for c in subcols] # get_loc returns the index of the column
         total_importance = sum(global_vals[i] for i in indices)
+        print(type(total_importance), total_importance)
         grouped.append({
             "feature": base_feature,
             "importance": round(total_importance, 3)
@@ -43,10 +45,20 @@ def group_global_shap_values(global_vals, columns):
 def compute_shap_values(start=0, end=5):
     model, X_test = load_model_and_test_data()
     shape_dict = {}
+    model_name = type(model).__name__
+
+    if model_name == 'RandomForestRegressor':
+        task = 'regression'
+    else:
+        task = 'classification'
 
     # Build a SHAP explainer object.
     explainer = shap.Explainer(model) # Explainer class automatically detects the model type
+    # baseline = explainer.expected_value[0] # baseline is the model's average prediction (if it could see 0 features, the model would predict this value)
+
+    # print(type(baseline))
     shap_values = explainer.shap_values(X_test) # shape: (num_rows, num_features) and is an np array
+    print(shap_values.shape, type(shap_values), shap_values)
     shape_dict["shap_values"] = shap_values.shape
 
     # Global importance (mean absolute SHAP value across rows)
@@ -54,9 +66,33 @@ def compute_shap_values(start=0, end=5):
     shape_dict["global_importances"] = global_importances.shape
     grouped_global_importances = group_global_shap_values(global_importances, X_test.columns)
 
+    # Local explanations: For the queried rows, corresponding shap values (from shap_values) are processed with a grouping logic and explanation is added as a dictionary.
+    local_explanations = []
+    mapping = get_feature_group_map(X_test.columns)
+
+    for i in range(start, min(end, len(X_test))):
+        row = X_test.iloc[i:i+1]
+        prediction = model.predict(row)[0]
+        shap_row = shap_values[i]
+
+        local_explanation = {}
+        # total = 0
+        # Sum of all feature's shap value for a prediction = deviation of model output from baseline
+        for feature, subcols in mapping.items():
+            feature_contribution = sum(shap_row[X_test.columns.get_loc(c)] for c in subcols)
+            print(type(feature_contribution))
+            local_explanation[feature] = round(feature_contribution, 3)
+            # total += feature_contribution
+        
+        local_explanations.append({
+            "row_index": i,
+            "prediction": prediction,
+            "shap_contributions": local_explanation,
+            # "total": total
+        })
+
     return {
         "model_type": type(model).__name__,
         "global_feature_importance": grouped_global_importances,
-        "shape_dict": shape_dict,
-        "n": len(grouped_global_importances)
+        "local_explanations": local_explanations,
     }
--- app/routes/controller.py
+++ app/routes/controller.py
@@ -49,7 +49,8 @@ async def train_endpoint(request: TrainRequest):
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+# Query parameters are are specified in the URL after a ?. so if not provided, it will default to 0 and 5.
 @router.get("/explain-model")
 async def explain_endpoint(start: int = Query(0), end: int = Query(5)):
     try:

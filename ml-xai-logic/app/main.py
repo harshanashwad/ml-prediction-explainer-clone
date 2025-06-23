e1db937974ec69f0c93425674f38d8f128eb789f
harshanashwad <harshanashwad@gmail.com>
2025-06-23 14:12:06 +0800

    fix(api): Enable CORS middleware for FastAPI app

--- ml-xai-logic/app/main.py
+++ ml-xai-logic/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI # class used to create my web application
 from app.routes import controller
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="ML Prediction Explanation Interface") # Initializing my app as an instance of the FastAPI class
+
+# Added to fix CORS middleware issue
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(controller.router)
 @app.get("/") # route decorator - home page of API

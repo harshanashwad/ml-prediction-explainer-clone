d1a36fd08c21391ede48a906580f9caf1027c072
harshanashwad <harshanashwad@gmail.com>
2025-06-23 14:46:43 +0800

    feat(fronted): Target Selector component (to be used while training model)

--- ml-xai-frontend/src/App.jsx
+++ ml-xai-frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import UploadCSV from "./components/UploadCSV";
+import TargetSelector from "./components/TargetSelector";
+
 
 function App() {
   const [csvSummary, setCsvSummary] = useState(null);
+  const [target, setTarget] = useState("");
 
   return (
     <div className="min-h-screen bg-base-200 p-8">
       {/* setCsvSummary(result) where result is the response received from making axios call to upload-csv/ in api backend */}
       <UploadCSV onUploadSuccess={setCsvSummary} />
 
-      {csvSummary && (
+      {csvSummary && ( 
+        <>
         <div className="mt-6 bg-white shadow-md p-4 rounded max-w-xl mx-auto">
           <h3 className="text-lg font-semibold mb-2">File Summary</h3>
           <p><strong>Rows:</strong> {csvSummary.row_count}</p>
@@ -24,6 +28,13 @@ function App() {
             </ul>
           </div>
         </div>
+
+        <TargetSelector
+            options={csvSummary.target_candidates}
+            selected={target}
+            onSelect={setTarget}
+          />
+      </>
       )}
     </div>
   );
